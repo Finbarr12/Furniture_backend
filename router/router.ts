5c6fd8270004f44782bd61f053ff7dc19b3d9686
finbarr12 <finarrogbonna@gmail.com>
2023-05-05 09:51:05 +0100

    routesadded

---
+++ router/router.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { SignInadmin, SignUpAdmin } from "../Controller/AdminController";
+import { Allproducts, CreateProducts } from "../Controller/ProductController";
+import ViewImage from "../config/Multer";
+import { Mailer } from "../Controller/Mailer";
+
+const router = Router();
+
+router.post("/adminsignup", SignUpAdmin);
+router.post("/adminsignup", SignInadmin);
+router.post("/createproduct", ViewImage, CreateProducts);
+router.get("/allproducts", Allproducts);
+router.post("mailer", Mailer);
+
+export default router;
