e9d033ad89914f4b514567394812c0562cb90127
finbarr12 <finarrogbonna@gmail.com>
2023-05-05 09:22:19 +0100

    prouctcontroller

---
+++ Controller/ProductController.ts
@@ -0,0 +1,47 @@
+import { Request, Response } from "express";
+import { Iproducts } from "../Interface/interface";
+import ProductModel from "../Model/ProductModel";
+import cloudinary from "../config/cloudinary";
+
+export const CreateProducts = async (
+  req: Request<{}, {}, Iproducts>,
+  res: Response
+) => {
+  try {
+    const { name, Image } = req.body;
+
+    const CloudImg = await cloudinary.uploader.upload(req!.file!.path);
+
+    const createProd = await ProductModel.create({
+      name,
+      Image: CloudImg!.secure_url,
+    });
+
+    return res.status(200).json({
+      message: "created",
+      data: createProd,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      message: "an error occured",
+      data: error,
+    });
+  }
+};
+
+//Allproducts
+
+export const Allproducts = async (req: Request, res: Response) => {
+  try {
+    const Products = await ProductModel.find();
+    res.status(200).json({
+      message: "Products gotten",
+      data: Products,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      message: "an error occured",
+      data: error,
+    });
+  }
+};
